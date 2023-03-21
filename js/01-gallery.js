2589f2d014b322921729bd19f2bbc288bce4ef63
8wondrs <8wondrs@gmail.com>
2023-03-21 19:27:04 +0200

    js-hw-07 - ✅

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', onImgClick);
+
+let instance;
+
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+});
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup.join(''));
+
+function onImgClick(event) {
+  if (event.target.nodeName !== 'IMG') return;
+  event.preventDefault();
+
+  instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+  `);
+
+  instance.show();
+
+  document.addEventListener('keydown', onEscPress);
+}
+
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', onEscPress);
+  }
+}
